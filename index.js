8a37656e5ac8d8e51337017bb9da65e2a2a95741
David Chapman <chap6680misc@gmail.com>
2018-01-20 07:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-// Add your doToElementsInArray() function here:
+function iterativeLog(array) {
+  array.forEach(logItemsToConsole)
+}
 
+function logItemsToConsole(item, index) {
+  console.log(`${index}: ${item}`);
+}
 
-// Add your changeCompletely() function here:
+function iterate(callback) {
+  var array = [1, 2, 3, 5, 8, 13];
+  
+  array.forEach(callback);
+  
+  return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
